b96e597c0e383935e8cd6ab8571b69d8373abce6
ozgurtuzgen <ozgurtuzgen@hotmail.com>
2015-04-30 08:35:46 +0300

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Created by otuzgen on 21.04.2015.
  */
 module.exports  = {
-    "database": "mongodb://ozgurtuzgen:19811984@ds043981.mongolab.com:43981/userstory",
+    "database": "mongodb://ozgurtuzgen:password@ds043981.mongolab.com:43981/userstory",
     "port": process.env.PORT || 3000,
     "secretKey": "SecretKey"
 }
